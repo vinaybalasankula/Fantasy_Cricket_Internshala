e288ef86e987c014437c06ba2949e72a86349271
vinaybalasankula <vinaybalasankula@gmail.com>
2021-05-28 22:07:47 +0530

    Project source file.

---
+++ tcs.py
@@ -0,0 +1,8 @@
+numberoftestcase=int(input())
+for i in len(numberoftestcase):
+	numberofemployee=int(input())
+for i in len(numberofemployee):
+	lis=(int(input()))
+print(numberoftestcase)
+print(numberofemployee)
+print(lis,end="")
